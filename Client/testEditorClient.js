a7444a1b1310f20be175e2ec1b191adca6a2b989
Alexander Durgin <alexander@Lambda.(none)>
2013-05-30 12:54:45 -0500

    edited editor

--- Client/testEditorClient.js
+++ Client/testEditorClient.js
@@ -7,7 +7,7 @@ var map1 = {
 	{
 		var xhr = new XMLHttpRequest()
 //		xhr.open("POST","http://128.104.191.51:8080/", true); //method, url, async
-		xhr.open("POST", window.location, true); //method, url, async
+		xhr.open("POST", "http://" + "localhost" + ":8080", true); //method, url, async
 		xhr.send(cm.getValue());
 	}
 };
@@ -34,7 +34,7 @@ document.getElementById("sendbutton").addEventListener("click", function()
 										updateText(xhr.responseText);
 									}
 								};
-						xhr.open("POST",window.location, true);
+						xhr.open("POST", "http://" + "localhost:" + "8080", true);
 						xhr.send(editor.getValue());
 					}
 );
